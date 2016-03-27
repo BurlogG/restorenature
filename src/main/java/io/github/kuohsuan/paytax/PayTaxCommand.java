66a2673ef981cd74970edb391933a16f237c1500
LogoCat <kuohsuanlo@gmail.com>
2016-03-28 05:07:29 +0800

    first commit

---
+++ src/main/java/io/github/kuohsuan/paytax/PayTaxCommand.java
@@ -0,0 +1,24 @@
+
+package io.github.kuohsuan.paytax;
+
+import net.milkbowl.vault.economy.EconomyResponse;
+
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ * Handler for the /pos sample command.
+ * @author SpaceManiac
+ */
+public class PayTaxCommand implements CommandExecutor {
+
+	public boolean onCommand(CommandSender arg0, Command arg1, String arg2,
+			String[] arg3) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
