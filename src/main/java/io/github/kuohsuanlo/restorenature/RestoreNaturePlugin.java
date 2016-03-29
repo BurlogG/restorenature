c3ca1007c4e632a3bf62931ec1f1187c3e0c0e37
LogoCat <kuohsuanlo@gmail.com>
2016-03-30 00:49:10 +0800

    progress to file

--- src/main/java/io/github/kuohsuanlo/restorenature/RestoreNaturePlugin.java
+++ src/main/java/io/github/kuohsuanlo/restorenature/RestoreNaturePlugin.java
@@ -2,9 +2,19 @@
 package io.github.kuohsuanlo.restorenature;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.logging.Logger;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import org.bukkit.Chunk;
 import org.bukkit.ChunkSnapshot;
@@ -20,23 +30,41 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class RestoreNaturePlugin extends JavaPlugin {
     private static final Logger log = Logger.getLogger("Minecraft");
-    public static int MAX_SECONDS_UNTOUCHED = 5;
+    public static int MAX_SECONDS_UNTOUCHED = 20;//864000 = 10 days
+    public static int CHECK_PERIOD_IN_SECONDS = 5;//
     public static int MAX_CHUNK_RADIUS = 3;
-    private ArrayList<String> arraylist_maintainedworlds_name = new  ArrayList<String> ();
+    private ArrayList<String> worlds_name = new ArrayList<String>();
+	public ArrayList<MapChunkInfo> maintained_worlds = new ArrayList<MapChunkInfo>();
     protected RestoreNatureRegularUpdate BukkitSchedulerSuck; 
     protected RestoreNatureBlockListener blockListener = new RestoreNatureBlockListener(this); 
-    
+    public static HashMap<String, String> messageData = new HashMap<String, String>();
     @Override
     public void onDisable() {
-        // TODO: Place any custom disable code here
-        // NOTE: All registered events are automatically unregistered when a plugin is disabled
-        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+    	for(int i=0;i<BukkitSchedulerSuck.maintained_worlds.size();i++){
+			try {
+	        	FileOutputStream fos;
+				fos = new FileOutputStream("./plugins/RestoreNature/worlds_chunk_info/"+BukkitSchedulerSuck.maintained_worlds.get(i).world_name+".chunkinfo");
+	        	ObjectOutputStream oos = new ObjectOutputStream(fos);
+	        	oos.writeObject(BukkitSchedulerSuck.maintained_worlds.get(i));
+	        	oos.close();
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    	
         getLogger().info(String.format("[%s] Disabled Version %s", getDescription().getName(), getDescription().getVersion()));
     }
 
     @Override
     public void onEnable() { 
+
     	
+    	readingConfig();
+        
     	PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(blockListener, this);
 
@@ -49,12 +77,74 @@ public class RestoreNaturePlugin extends JavaPlugin {
         enablingWorlds();
         startingRestoreRoutines();
     }
+    private void readingConfig(){
+    	new File("./plugins/RestoreNature").mkdirs();
+
+	    worlds_name.add("world");     	
+    }
     private void enablingWorlds(){
-        arraylist_maintainedworlds_name.add("wasteland"); 
+    	MapChunkInfo world_chunk_info;
+	
+        new File("./plugins/RestoreNature/worlds_chunk_info").mkdirs();        
+
+        for(int i =0;i<worlds_name.size();i++){
+        	String world_name = worlds_name.get(i);
+    		try {
+    	  	   	String path = "./plugins/RestoreNature/worlds_chunk_info/"+world_name+".chunkinfo";
+    	  	   	File file = new File(path);
+    	  	   	
+    	  	   	
+    	  	   	if(file.exists()){
+    	  	   		this.getServer().getConsoleSender().sendMessage("¡±e[RestoreNature] : successfully load from "+"./plugins/RestoreNature/worlds_chunk_info/"+world_name+".chunkinfo");	
+    	  	   		
+    				FileInputStream fis;
+    				fis = new FileInputStream("./plugins/RestoreNature/worlds_chunk_info/"+world_name+".chunkinfo");
+    		    	ObjectInputStream ois = new ObjectInputStream(fis);
+    		    	world_chunk_info = (MapChunkInfo) ois.readObject();
+    		    	ois.close();
+    	  	   	}
+    	  	   	else{
+    	  	   		this.getServer().getConsoleSender().sendMessage("¡±e[RestoreNature] : no existing file, creating new chunk info");
+    				world_chunk_info = createMapChunkInfo(world_name);
+
+    	  	   	}
+
+    	    	maintained_worlds.add(world_chunk_info);
+    		} catch (FileNotFoundException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		} catch (IOException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		} catch (ClassNotFoundException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+        }
+
+
+    }
+    private MapChunkInfo createMapChunkInfo(String world_name){
+    	
+       	int min_chunk_x = 0;
+       	int min_chunk_z = 0; 
+       	int max_chunk_x = MAX_CHUNK_RADIUS*2;
+       	int max_chunk_z = MAX_CHUNK_RADIUS*2; 
+
+    	int[][] world_chunks = new int[max_chunk_x+1][max_chunk_z+1];
+    	for (int x=min_chunk_x; x <= max_chunk_x; x++){
+		   for (int z=min_chunk_z; z <= max_chunk_z; z++){
+			   world_chunks[x][z] = 0;
+		   }
+    	}
+    	return new MapChunkInfo(world_name,world_chunks);
+    	
+    	
+    	
     }
     private void startingRestoreRoutines(){
-        BukkitSchedulerSuck = new RestoreNatureRegularUpdate(1,MAX_SECONDS_UNTOUCHED,MAX_CHUNK_RADIUS,arraylist_maintainedworlds_name,this);
-        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, BukkitSchedulerSuck, 0, 20);
+        BukkitSchedulerSuck = new RestoreNatureRegularUpdate(CHECK_PERIOD_IN_SECONDS,MAX_SECONDS_UNTOUCHED,MAX_CHUNK_RADIUS,maintained_worlds,this);
+        this.getServer().getScheduler().scheduleSyncRepeatingTask(this, BukkitSchedulerSuck, 0, 20*CHECK_PERIOD_IN_SECONDS);
 
     }
 }
--- src/main/java/io/github/kuohsuanlo/restorenature/RestoreNatureRegularUpdate.java
+++ src/main/java/io/github/kuohsuanlo/restorenature/RestoreNatureRegularUpdate.java
@@ -2,6 +2,7 @@
 
 package io.github.kuohsuanlo.restorenature;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
 import org.bukkit.Chunk;
@@ -14,60 +15,57 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 
-class RestoreNatureRegularUpdate implements Runnable {
+class MapChunkInfo implements Serializable {
+	public int[][] chunk_untouchedtime;
+	public String world_name;
+	public MapChunkInfo(String name,int[][] time){
+		chunk_untouchedtime = time;
+		world_name = name;
+	}
 
-	public ArrayList<int[][]> arraylist_chunk_untouchedtime;
-	public ArrayList<String> arraylist_maintainedworlds_name = new ArrayList<String>();
+} 
+class RestoreNatureRegularUpdate implements Runnable {
 	private int max_time_in_seconds;
 	private int period_in_seconds;
 	private RestoreNaturePlugin rnplugin;
+	public ArrayList<MapChunkInfo> maintained_worlds = new ArrayList<MapChunkInfo>();
 	private int min_chunk_x;
 	private int min_chunk_z;
 	private int max_chunk_x;
 	private int max_chunk_z;
 	private int chunk_radius;
 
-    public RestoreNatureRegularUpdate(int period,int max_time,int radius,ArrayList<String> worlds_name,RestoreNaturePlugin plugin) {
+    public RestoreNatureRegularUpdate(int period,int max_time,int radius,ArrayList<MapChunkInfo> existing_worlds,RestoreNaturePlugin plugin) {
     	max_time_in_seconds = max_time;
     	period_in_seconds = period;
-    	arraylist_maintainedworlds_name = worlds_name;
+    	chunk_radius = radius; 
+    	
     	rnplugin= plugin;
+       	min_chunk_x = 0;
+       	min_chunk_z = 0; 
+       	max_chunk_x = radius*2;
+       	max_chunk_z = radius*2; 
+ 
     	
-    	min_chunk_x = 0;
-    	min_chunk_z = 0; 
-    	max_chunk_x = radius*2+1;
-    	max_chunk_z = radius*2+1; 
-    	
-    	chunk_radius = radius;
-    	
-    	arraylist_chunk_untouchedtime = new ArrayList<int[][]>();
-    	
-    	for(int i=0;i<arraylist_maintainedworlds_name.size();i++){
-        	int[][] world_chunks = new int[max_chunk_x][max_chunk_z];
-        	for (int x=min_chunk_x; x < max_chunk_x; x++){
-    		   for (int z=min_chunk_z; z < max_chunk_z; z++){
-    			   world_chunks[x][z] = 0;
-    		   }
-        	}
-    		arraylist_chunk_untouchedtime.add(i, world_chunks);
-    	}
+    	maintained_worlds = existing_worlds;
+
 
     }
     public void run() {
-    	rnplugin.getServer().getConsoleSender().sendMessage("¡±eChunk successfully start restoration calculation");			    
-    	for(int i=0;i<arraylist_maintainedworlds_name.size();i++){
-        	for (int x=min_chunk_x; x < max_chunk_x; x++){
-    		   for (int z=min_chunk_z; z < max_chunk_z; z++){
-    			   
-    	        	arraylist_chunk_untouchedtime.get(i)[x][z]+=period_in_seconds;
-    	        	
-    	        	if(arraylist_chunk_untouchedtime.get(i)[x][z]>=max_time_in_seconds){
-    	        		//Triggering console restorenature
-    	        		int chunk_x = x-chunk_radius;
-    	        		int chunk_z = z-chunk_radius;
-    	        		rnplugin.getServer().dispatchCommand(rnplugin.getServer().getConsoleSender(), "restorenature "+arraylist_maintainedworlds_name.get(i)+" "+chunk_x+" "+chunk_z);
-    	        		arraylist_chunk_untouchedtime.get(i)[x][z]=0;
-    	        	}
+    	rnplugin.getServer().getConsoleSender().sendMessage("¡±e[RestoreNature] : Total # worlds : "+maintained_worlds.size());	
+    	for(int i=0;i<maintained_worlds.size();i++){
+        	for(int x=min_chunk_x; x <= max_chunk_x; x++){
+    		    for(int z=min_chunk_z; z <= max_chunk_z; z++){
+    		    	rnplugin.getServer().getConsoleSender().sendMessage("¡±e[RestoreNature] : Checking "+maintained_worlds.get(i).world_name+" "+Integer.toString(x-chunk_radius)+" ; "+Integer.toString(z-chunk_radius));	
+					maintained_worlds.get(i).chunk_untouchedtime[x][z]+=period_in_seconds;
+					
+					if(maintained_worlds.get(i).chunk_untouchedtime[x][z]>=max_time_in_seconds){
+						//Triggering console restorenature
+						int chunk_x = x-chunk_radius;
+						int chunk_z = z-chunk_radius;
+						rnplugin.getServer().dispatchCommand(rnplugin.getServer().getConsoleSender(), "restorenature "+maintained_worlds.get(i).world_name+" "+chunk_x+" "+chunk_z);
+						maintained_worlds.get(i).chunk_untouchedtime[x][z]=0;
+					}
     	        	
     			}
     		}
@@ -75,11 +73,11 @@ class RestoreNatureRegularUpdate implements Runnable {
     	}
     }
 	public void setWorldsChunkUntouchedTime(Block touched_block){
-    	for(int i=0;i<arraylist_maintainedworlds_name.size();i++){
-    		if(arraylist_maintainedworlds_name.get(i).equals(touched_block.getWorld().getName())){
+    	for(int i=0;i<maintained_worlds.size();i++){
+    		if(maintained_worlds.get(i).world_name.equals(touched_block.getWorld().getName())){
     			int x = touched_block.getChunk().getX()+chunk_radius;
     			int z = touched_block.getChunk().getZ()+chunk_radius;
-    			arraylist_chunk_untouchedtime.get(i)[x][z] = 0;
+    			maintained_worlds.get(i).chunk_untouchedtime[x][z] = 0;
     		}
     	}
 	
