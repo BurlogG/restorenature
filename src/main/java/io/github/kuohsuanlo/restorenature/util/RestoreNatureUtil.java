cab6c60ba0990ac0987b12b628fe68ea40e15832
kuohsuanlo <kuohsuanlo@gmail.com>
2018-01-10 11:06:34 +0800

    merged.

--- src/main/java/io/github/kuohsuanlo/restorenature/util/RestoreNatureUtil.java
+++ src/main/java/io/github/kuohsuanlo/restorenature/util/RestoreNatureUtil.java
@@ -70,12 +70,8 @@ public class RestoreNatureUtil {
 			restored_chest.update();
 		}
 	}
-<<<<<<< HEAD
-	private static int[] calculateChunkEntityTypesNumber(Chunk chunk){
-		int dm = ENTITY_CAL_RADIUS;
-=======
 	private static int[] calculateChunkEntityTypesNumber(Chunk chunk, int dm){
->>>>>>> origin/master
+		
 		int[] entityNum = new int[EntityType.values().length];
 		
 		for(int dx=-dm;dx<=dm;dx++){
@@ -135,7 +131,7 @@ public class RestoreNatureUtil {
 		if(!restored_chunk.isLoaded()) 	restored_chunk.load();
 		
 		int restoredEntityNumbers=0;
-		int[] entityNum = calculateChunkEntityTypesNumber(restored_chunk,1);
+		int[] entityNum = calculateChunkEntityTypesNumber(restored_chunk, ENTITY_CAL_RADIUS);
 		
 		//restoring missing entities in restored chunk from restoring chunk
 		Entity[] entitiesRestoring = restoring_chunk.getEntities();
